d80ee4a40f56f9a6c3712583b45a8b2829783d01
ceniza-sjs <stevjocen@outlook.ph>
2020-11-02 17:20:56 +0800

    Implement database configuration.

--- src/minimart/settings.py
+++ src/minimart/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for minimart project.
 
@@ -73,11 +74,13 @@ WSGI_APPLICATION = 'minimart.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':os.environ.get('DB_HOST'),
+        'NAME':os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
     }
 }
 
