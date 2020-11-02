80f148da3a3b8752d496fc7ef20f8793431f2d1a
ceniza-sjs <stevjocen@outlook.ph>
2020-11-02 18:06:54 +0800

    Allow docker toolbox localhost.

--- src/minimart/settings.py
+++ src/minimart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-br%)kv+bord7(!_nbk1&yg=cd=c--h+)fm1jl)0kt)f0ce!v)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100']
 
 
 # Application definition
